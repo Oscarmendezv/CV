54a7affec6975f12283ea7e6d846fcc50b084307
Óscar Méndez <oscar.mendez-valladares@akka.eu>
2019-06-10 22:38:49 +0200

    Adding welcome page + menu

---
+++ my-cv/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
